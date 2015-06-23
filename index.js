e9ace66e0e7d0376401706e2f0417ec9b3cee6eb
Boopathi Rajaa <me@boopathi.in>
2015-06-23 20:24:01 +0530

    Fix some stuff and stuck at remote ls

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var DEBUG = true;
 var FFI = require('ffi');
 var ref = require('ref');
 var path = require('path');
+var Struct = require('ref-struct');
 
 var Repo = ref.types.void;
 var RepoPtr = ref.refType(Repo);
@@ -13,6 +14,13 @@ var RepoPtrPtr = ref.refType(RepoPtr);
 var Remote = ref.types.void;
 var RemotePtr = ref.refType(Remote);
 var RemotePtrPtr = ref.refType(RemotePtr);
+
+// var RemoteHead = Struct({
+//   local: 'int',
+//   git_oid: ref.types.void,
+//   name: 'string',
+//   symref_target: 'string'
+// });
 
 var RemoteHead = ref.types.void;
 var RemoteHeadPtr = ref.refType(RemoteHead);
@@ -31,9 +39,7 @@ var libgit2 = FFI.Library('/Users/boopathi.rajaa/workspace/libgit2/build/libgit2
 
 var err;
 
-err = libgit2.git_libgit2_init();
-if (err) return console.log('global init failure');
-if (DEBUG) console.log('global init success');
+libgit2.git_libgit2_init();
 
 var repoPtrPtr = ref.alloc(RepoPtrPtr);
 err = libgit2.git_repository_open(repoPtrPtr, path.resolve('./'));
@@ -44,8 +50,17 @@ var repoPtr = repoPtrPtr.deref();
 var repo = repoPtr.deref();
 
 var remotePtrPtr = ref.alloc(RemotePtrPtr);
-err = libgit2.git_remote_lookup(remote, repoPtr, 'origin')
+err = libgit2.git_remote_lookup(remotePtrPtr, repoPtr, 'origin')
 if (err) return console.log('failed to lookup remote');
 if (DEBUG) console.log('remote lookup success');
+
+var remotePtr = remotePtrPtr.deref();
+var remote = remotePtr.deref();
+
+var remoteHeadPtrPtrPtr = ref.alloc(RemoteHeadPtrPtrPtr);
+var sizePtr = ref.alloc(SizeTPtr);
+err = libgit2.git_remote_ls(remoteHeadPtrPtrPtr, sizePtr, remotePtr);
+if (err) return console.log('remote ls failed');
+if (DEBUG) console.log('remote ls success');
 
 console.log('success');
