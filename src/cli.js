818d4f3893021f483cec7a16380e67d77fdce38a
Boopathi Rajaa <me@boopathi.in>
2015-06-24 23:48:22 +0530

    Add verbose output and option to pass remote from cli

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,38 @@
 #!/usr/bin/env node
 
 import VerifyGitTag from '../';
+import program from 'commander';
+import {version, name} from '../package.json';
+import 'colors';
+
 let DEBUG = false;
 
 if (typeof process.env.DEBUG !== 'undefined')
   DEBUG = ['true', '1'].indexOf(process.env.DEBUG.toString().toLowerCase()) !== -1;
 
-VerifyGitTag()
-  .then(isExist => process.exit(!isExist))
+program
+  .version(version)
+  .option('-v, --verbose', 'Verbose output')
+  .option('-r, --remote <remote>', 'git remote')
+  .parse(process.argv);
+
+let opts = {
+  remote: program.remote
+};
+
+let green = name.green;
+let red = name.red;
+
+let successMessage = `[${green}] tag v${version} exists`;
+let failureMessage = `[${red}] tag v${version} does not exist
+[${red}] ` + `git tag v${version} && git push --tags`;
+
+VerifyGitTag(opts)
+  .then(isExist => {
+    if (program.verbose)
+      console.log(isExist ? successMessage : failureMessage);
+    process.exit(!isExist)
+  })
   .catch(err => {
     if (DEBUG) console.error(err.stack);
     process.exit(2);
